5f1923048996d4b2e66e52e4f3897089be6d207b
Mahesh Marupaka <maheshmarupaka473@gmail.com>
2023-05-03 18:14:16 +0530

    changes in invoices.vue

--- src/views/Invoices.vue
+++ src/views/Invoices.vue
@@ -3,3 +3,12 @@
         <h1>Invoices</h1>
     </div>
 </template>
+<script setup>
+import {onBeforeRouteLeave} from 'vue-router'
+onBeforeRouteLeave(()=>{
+    // console.log('to',to)
+    // console.log('from',from)
+    const answer = window.confirm('Are you sure you  want to leave? Invoices are super awesome!')
+    if(!answer) return false
+})
+</script>
